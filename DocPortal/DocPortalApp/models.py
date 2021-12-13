f8ece398f6e269489b59f8694207cbff6cc0ec33
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-12-13 17:39:58 +0400

    Added Auth&Reg Module

--- DocPortal/DocPortalApp/models.py
+++ DocPortal/DocPortalApp/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class File_Type(models.Model):
+    name = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+class User_Doc(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    fileName = models.CharField(max_length=255)
+    fileType = models.ForeignKey(File_Type, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='docs/')
+    fileSize = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+    # def delete(self, *args, **kwargs):
+    #     self.file.delete()
+    #     super().delete(*args, **kwargs)
--- DocPortal/DocPortalApp/urls.py
+++ DocPortal/DocPortalApp/urls.py
@@ -1,6 +1,16 @@
+from os import stat
+from django.conf import settings
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('', views.index),
+    path('', views.index, name='index'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/register/', views.register_view, name='register'),
+    path('accounts/logout/', views.logout_view, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
